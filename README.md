c3720cf57b15875a934b95b078d1d742146ac922
Eduardo Alves <edwardalves866@gmail.com>
2022-08-01 13:52:36 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Red_dead_2
+ Lading Page de Red Dead redemption 2
---
+++ red.js
@@ -0,0 +1,7 @@
+function mudafoto (foto) {
+
+    document.getElementById("icone").src = foto
+}
+function mudacarta (foto) {
+    document.getElementById("icone2").src = foto
+}
